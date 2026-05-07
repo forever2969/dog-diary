3e074d4e69c38d376e3e957104602f101afef066
forever2969 <forever296@naver.com>
2026-05-07 15:40:55 +0900

    include pong.jpeg in lambda bundle for icon routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    '/icon': ['./public/pong.jpeg'],
+    '/apple-icon': ['./public/pong.jpeg'],
+  },
 };
 
 export default nextConfig;
